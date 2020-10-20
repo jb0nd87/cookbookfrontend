b5ec87498ded25d506051e8032843c3f34334022
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-10-19 21:45:34 -0700

    cookbook frontend update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import Display from './Display';
+import Form from './Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const url = 'http://localhost:3000';
+	
+	const [cookbooks, setCookbooks] = React.useState([]);
+	
+	const emptyCookbook = {
+		title: '',
+		yearPublished: '',
+	};
 
+	
+	const [selectedCookbook, setSelectedCookbook] = React.useState(emptyCookbook);
+
+	
+	const getCookbooks = () => {
+		fetch(url + '/cookbook/')
+			.then((response) => response.json())
+			.then((data) => {
+				setCookbooks(data);
+			});
+	};
+
+	
+	React.useEffect(() => getCookbooks(), []);
+
+
+	const handleCreate = (newCookbook) => {
+		fetch(url + '/cookbook/', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newCookbook),
+		}).then((response) => getCookbooks());
+	};
+
+	
+	const handleUpdate = (cookbook) => {
+		fetch(url + '/cookbook/' + cookbook._id, {
+			method: 'put',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(cookbook),
+		}).then((response) => getCookbooks());
+	};
+
+	
+	const selectCookbook = (cookbook) => {
+		setSelectedCookbook(cookbook);
+	};
+
+	
+	const deleteCookbook = (cookbook) => {
+		fetch(url + '/cookbook/' + cookbook._id, {
+			method: 'delete',
+		}).then((response) => getCookbooks());
+	};
+
+	return (
+		<div className='App'>
+			<h1>COOKBOOKS</h1>
+			<hr />
+			<Link to='/create'>
+				<button>Add Author</button>
+			</Link>
+			<main>
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={(rp) => (
+							<Display
+								{...rp}
+								cookbooks={cookbooks}
+								selectCookbook={selectCookbook}
+								deleteCookbook={deleteCookbook}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path='/create'
+						render={(rp) => (
+							<Form
+								{...rp}
+								label='create'
+								cookbook={emptyCookbook}
+								handleSubmit={handleCreate}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path='/edit'
+						render={(rp) => (
+							<Form
+								{...rp}
+								label='update'
+								cookbook={selectedCookbook}
+								handleSubmit={handleUpdate}
+							/>
+						)}
+					/>
+				</Switch>
+			</main>
+		</div>
+	);
+}
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,36 @@
 import React from 'react';
 
+const Form = (props) => {
+	
+	const [formData, setFormData] = React.useState(props.cookbook);
+
+	const handleSubmit = (event) => {
+		event.preventDefault(); 
+		props.handleSubmit(formData); 
+		props.history.push('/'); 
+	};
+
+	const handleChange = (event) => {
+		setFormData({ ...formData, [event.target.title]: event.target.value });
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='text'
+				name='title'
+				value={formData.title}
+				onChange={handleChange}
+			/>
+			<input
+				type='number'
+				name='yearPublished'
+				value={formData.yearPublished}
+				onChange={handleChange}
+			/>
+			<input type='submit' value={props.label} />
+		</form>
+	);
+};
 
 export default Form;
